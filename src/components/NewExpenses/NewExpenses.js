57049925083f45b7d84c302a50259818da1fbd00
[Shmuel Bokobza] <[samuel.bokobza@gmail.com]>
2022-07-18 17:34:55 +0300

    Add Ternary on AddExpense

--- src/components/NewExpenses/NewExpenses.js
+++ src/components/NewExpenses/NewExpenses.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showExpenseForm, setShowExpenseForm] = useState(false);
   const saveExpenseDataHandler = (formSubmit) => {
     const expenseData = {
       ...formSubmit,
       id: Math.random().toString(),
     };
     props.onFormSubmit(expenseData);
+    setShowExpenseForm(false);
   };
+  const isShowingFormHandler = () => {
+    setShowExpenseForm(true);
+  };
+  const isNoShowingFormHandler = () => {
+    setShowExpenseForm(false);
+  };
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showExpenseForm && (
+        <button onClick={isShowingFormHandler}> Add Expense</button>
+      )}
+      {showExpenseForm && (
+        <ExpenseForm
+          onCancel={isNoShowingFormHandler}
+          onSaveExpenseData={saveExpenseDataHandler}
+        />
+      )}
     </div>
   );
 };
